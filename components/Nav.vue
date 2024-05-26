08cfe60fee66d9cbec3284eb78715173908e8bd0
zhen <770226915@qq.com>
2024-05-26 12:54:33 +0800

    chore

---
+++ components/Nav.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <nav>
+    <div>SinceThen</div>
+    <div>darkmode</div>
+    <div>github</div>
+  </nav>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <main>
+    <Nav />
+  </main>
+</template>
